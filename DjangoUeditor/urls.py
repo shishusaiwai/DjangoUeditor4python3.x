960634ae18754ea681365eb1167d799fd1d52a31
shishusaiwai <shishusaiwai@vip.qq.com>
2016-07-13 19:03:49 +0800

    bug fix

--- DjangoUeditor/urls.py
+++ DjangoUeditor/urls.py
@@ -1,13 +1,12 @@
 # coding:utf-8
 from django import VERSION
-from .widgets import UEditorWidget, AdminUEditorWidget
 from .views import get_ueditor_controller
 
 if VERSION[0:2] > (1, 3):
-    from django.conf.urls import patterns, url
+    from django.conf.urls import url
+    urlpatterns = [url(r'^controller/$', get_ueditor_controller)]
 else:
     from django.conf.urls.defaults import patterns, url
-
-urlpatterns = patterns('',
-                       url(r'^controller/$', get_ueditor_controller)
-                       )
+    urlpatterns = patterns('',
+        url(r'^controller/$', get_ueditor_controller)
+    )
